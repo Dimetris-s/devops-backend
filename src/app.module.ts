41dbbb8e10154b56c7cbbf32e833db3eed181d7a
abazunts <bazunc@gmail.com>
2026-06-15 12:05:08 +0400

    added metrics.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,28 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {MetricsModule} from "./metrics.module";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: false,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        MetricsModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            autoLoadEntities: true,
+            synchronize: false,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor() {
+        console.log(process.env.DATABASE_URL)
+    }
+}
---
+++ src/metrics.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import {MetricsController} from "./metrics.controller";
+import {collectDefaultMetrics} from "prom-client";
+
+@Module({
+  imports: [
+  ],
+  controllers: [MetricsController],
+  providers: [],
+})
+export class MetricsModule {
+  constructor() {
+    collectDefaultMetrics()
+  }
+}
