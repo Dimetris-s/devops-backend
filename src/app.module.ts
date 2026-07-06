2755b7332c5d05c09b22677b1c563b52e48d3442
Дмитрий Мудрук <dmytro@MacBook-Air-Dmitrij.local>
2026-07-06 23:28:19 +0300

    set project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MetricsModule } from './metrics.module';
 import { ConfigModule } from '@nestjs/config';
-import { MetricsMiddleware } from './metrics.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MetricsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
@@ -22,11 +19,4 @@ import { MetricsMiddleware } from './metrics.middleware';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(MetricsMiddleware)
-      .exclude('metrics') // не измеряем сам /metrics эндпоинт
-      .forRoutes('*');
-  }
-}
+export class AppModule {}
