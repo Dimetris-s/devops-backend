c073115f249e8a6ddb708255836d3587bb319121
Дмитрий Мудрук <dmytro@MacBook-Air-Dmitrij.local>
2026-07-15 22:48:17 +0300

    update version

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -20,7 +20,7 @@ describe('AppController', () => {
       if (process.version.startsWith('v25')) {
         throw new Error('Node < 25!!');
       }
-      expect(appController.getHello()).toBe('Hello DevOps v5.1');
+      expect(appController.getHello()).toBe('Hello DevOps v5.2');
     });
   });
 });
